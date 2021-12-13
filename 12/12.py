a3d67db7c5a73c6e41ea31d75f2dd6476e2fc825
Dávid Slivka <david.slivka.sk@gmail.com>
2021-12-13 21:20:38 +0100

    renaming function

--- 12/12.py
+++ 12/12.py
@@ -12,7 +12,6 @@ with open('input12.txt', 'r') as f:
         points[to_point].append(from_point)
 
 
-
 def part1(path):
     global total
     if path[-1] == 'end':
@@ -28,7 +27,7 @@ def part1(path):
         part1(path + [x])
 
 
-def paths2(path, double=''):
+def part2(path, double=''):
     global total
     if path[-1] == 'end':
         total += 1
@@ -36,12 +35,12 @@ def paths2(path, double=''):
 
     for x in points[path[-1]]:
         if x.isupper() or x not in path:
-            paths2(path + [x], double)
+            part2(path + [x], double)
 
     if double == '':
         for x in points[path[-1]]:
             if x.islower() and x in path and x != 'start':
-                paths2(path + [x], x)
+                part2(path + [x], x)
 
 
 total = 0
@@ -49,5 +48,5 @@ part1(['start'])
 print(total)
 
 total = 0
-paths2(['start'], '')
+part2(['start'], '')
 print(total)
